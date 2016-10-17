a5168eab6b7a36c94c8f3c5ca841f92594691d50
enhui zhu <enhuizhu@Dxis-Mac-mini.local>
2016-10-17 17:33:34 +0100

    update

---
+++ app/components/loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Loader extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (<div>
+		</div>);
+	}
+}
+
+Loader.propTypes = {
+};
+
+Loader.defaultProps = {
+};
+
+export default Loader;
