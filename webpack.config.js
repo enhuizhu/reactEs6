972a29b66d6ba80c027e22c5ca699f7fca4d3068
rocabee-david <david@rocabee.com>
2016-12-18 21:32:05 +0000

    update webpack.config.js, let it can generate minified code

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,19 @@ var webpack = require('webpack');
 module.exports = {
   entry: './App.js',
   output: { path: __dirname + '/public/', filename: 'bundle.js' },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ],
   module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015','react']
-                }
+    loaders: [
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015','react']
             }
-        ]
-    }
+        }
+    ]
+ }
 };
